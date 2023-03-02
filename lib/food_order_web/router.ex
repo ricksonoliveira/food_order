c0f6760c9029c1a96c37d191405b9090081bfd52
ricksonoliveira <riksongleison471@gmail.com>
2023-03-02 19:27:06 -0300

    imprv: adding tailwind to mix deps and adding its deps

--- lib/food_order_web/router.ex
+++ lib/food_order_web/router.ex
@@ -5,7 +5,7 @@ defmodule FoodOrderWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, {FoodOrderWeb.LayoutView, :root}
+    plug :put_root_layout, {FoodOrderWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
