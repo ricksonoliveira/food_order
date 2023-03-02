20a2a3a2e253142bc6440de03c21fc4335b2731d
ricksonoliveira <riksongleison471@gmail.com>
2023-03-02 19:37:09 -0300

    fix: add mix assets task to dockerfile

--- mix.exs
+++ mix.exs
@@ -78,11 +78,9 @@ defmodule FoodOrder.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": [
-        "cmd --cd assets npm run deploy",
-        "esbuild default --minify",
-        "phx.digest"
-      ]
+      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.build": ["tailwind default", "esbuild default"],
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
