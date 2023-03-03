fa3d9e0a4ebfe5ff6728b9b17bc2c5ba32fa3d26
ricksonoliveira <riksongleison471@gmail.com>
2023-03-03 18:26:54 -0300

    feat: implement client page

--- lib/food_order/application.ex
+++ lib/food_order/application.ex
@@ -14,6 +14,8 @@ defmodule FoodOrder.Application do
       FoodOrderWeb.Telemetry,
       # Start the PubSub system
       {Phoenix.PubSub, name: FoodOrder.PubSub},
+      # Start Finch
+      {Finch, name: FoodOrder.Finch},
       # Start the Endpoint (http/https)
       FoodOrderWeb.Endpoint
       # Start a worker by calling: FoodOrder.Worker.start_link(arg)
