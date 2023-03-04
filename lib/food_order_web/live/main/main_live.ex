5b4f7044c1f5191792c63b4fe7e7a308a22ed402
Rickson G. Oliveira <riksongleison471@gmail.com>
2023-03-04 20:50:09 -0300

    Feat/live view lifecycle (#7)
    * feat: add component client
    
    * feat: add CI status badge
    
    * fix: mix format, credo

--- lib/food_order_web/live/main/main_live.ex
+++ lib/food_order_web/live/main/main_live.ex
@@ -1,7 +1,14 @@
 defmodule FoodOrderWeb.MainLive do
   use FoodOrderWeb, :live_view
+  alias FoodOrderWeb.Client
+  alias FoodOrderWeb.Main.ClientLive
 
   def mount(_assigns, _session, socket) do
-    {:ok, socket |> assign(name: "Rick", age: 25)}
+    {:ok, assign(socket, clients: Client.all())}
+  end
+
+  def handle_info({:change_name, id, name}, socket) do
+    send_update(ClientLive, id: id, name: name)
+    {:noreply, socket}
   end
 end
