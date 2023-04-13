28437ba31202929ff7c79748ffbe98c1c586cb0c
ricksonoliveira <riksongleison471@gmail.com>
2023-04-13 11:44:34 -0300

    feat: add simple form to add new products

--- test/food_order_web/live/admin/product_live/index_test.exs
+++ test/food_order_web/live/admin/product_live/index_test.exs
@@ -17,6 +17,21 @@ defmodule FoodOrderWeb.Admin.ProductLive.IndexTest do
       assert has_element?(view, product_id_el <> ">td>div>span", product.name)
       assert has_element?(view, product_id_el <> ">td>div>span", Atom.to_string(product.size))
     end
+
+    test "add new product", %{conn: conn} do
+      {:ok, view, _html} = live(conn, ~p"/admin/products")
+
+      assert view |> element("header>div>a", "New Product") |> render_click()
+
+      assert_patch(view, ~p"/admin/products/new")
+
+      assert view |> has_element?("#new-product-modal")
+
+      # open_browser(view)
+      assert view
+              |> form("#product-form", product: %{})
+              |> render_change() =~ " can't be blank"
+    end
   end
 
   defp create_product(_conn) do
