7fcd7ecebdcb4b1a3d00b6c7cdf4f80ce4fca1c4
Rickson G. Oliveira <riksongleison471@gmail.com>
2023-06-02 14:43:52 -0300

    feat: edit products (#9)

--- lib/food_order_web/live/admin/product_live/form.ex
+++ lib/food_order_web/live/admin/product_live/form.ex
@@ -14,10 +14,10 @@ defmodule FoodOrderWeb.Admin.ProductLive.Form do
       >
         <.input field={@form[:name]} label="Name" />
         <.input field={@form[:description]} type="textarea" label="Description" />
-        <.input field={@form[:price]} type="number" label="Price" />
+        <.input field={@form[:price]} label="Price" />
 
         <:actions>
-          <.button phx-disable-with="Saving...">Create Product</.button>
+          <.button phx-disable-with="Saving...">Save Product</.button>
         </:actions>
       </.simple_form>
     </div>
@@ -43,11 +43,29 @@ defmodule FoodOrderWeb.Admin.ProductLive.Form do
   end
 
   def handle_event("save", %{"product" => product_params}, socket) do
-    case Products.create_product(product_params) do
+    save(socket, socket.assigns.action, product_params)
+  end
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
+
+  defp save(socket, :new, product_params) do
+    Products.create_product(product_params)
+    |> perform_save_action(socket, "Product created successfully!")
+  end
+
+  defp save(socket, :edit, product_params) do
+    Products.update_product(socket.assigns.product, product_params)
+    |> perform_save_action(socket, "Product updated successfully!")
+  end
+
+  defp perform_save_action(action_result, socket, message) do
+    case action_result do
       {:ok, _} ->
         socket =
           socket
-          |> put_flash(:info, "Product created successfully!")
+          |> put_flash(:info, message)
           |> push_navigate(to: socket.assigns.navigate)
 
         {:noreply, socket}
@@ -55,9 +73,5 @@ defmodule FoodOrderWeb.Admin.ProductLive.Form do
       {:error, changeset} ->
         {:noreply, assign_form(socket, changeset)}
     end
-  end
-
-  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
-    assign(socket, :form, to_form(changeset))
   end
 end
