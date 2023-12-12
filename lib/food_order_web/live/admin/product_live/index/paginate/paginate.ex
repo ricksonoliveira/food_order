3ccef3313788482a99d031ff17d4913e86c3a7dc
Rick G Oliveira <riksongleison471@gmail.com>
2023-12-12 19:35:30 -0300

    feat: logic to navigate pages

--- lib/food_order_web/live/admin/product_live/index/paginate/paginate.ex
+++ lib/food_order_web/live/admin/product_live/index/paginate/paginate.ex
@@ -5,24 +5,23 @@ defmodule FoodOrderWeb.Admin.ProductLive.Index.Paginate do
     ~H"""
     <div id={@id} class="flex pr-4" >
       <div class="h-8 w-8 mr-1 flex justify-center items center cursor-pointer">
-        <a href="">
+        <.link :if={@options[:page] > 1} patch={~p"/admin/products?#{Map.update(@options, :page, @options[:page], &(&1 - 1))}"}>
           <Heroicons.chevron_left solid class="h-6 w-6 stroke-current text-fuchsia-500" />
-        </a>
+        </.link>
       </div>
 
       <div class="flex h-8 font-medium">
-        <div class="w-8 md:flex justify-center items-center hidden cursor-pointer leading-5 transition duration-150 ease-in border-b-2 border-fuchsia-500">
-          <a href="">1</a>
-        </div>
-        <div class="w-8 md:flex justify-center items-center hidden cursor-pointer leading-5 transition duration-150 ease-in border-b-2 border-b-transparent">
-          <a href="">2</a>
+        <div :for={current_page <- (@options[:page] - 2)..(@options[:page] + 1)} :if={current_page > 0} class={[current_page == @options[:page] && "border-fuchsia-500" || "border-transparent",  "w-8 md:flex justify-center items-center hidden cursor-pointer leading-5 transition duration-150 ease-in border-b-2"]}>
+         <.link :if={@options[:page] > 1} patch={~p"/admin/products?#{Map.put(@options, :page, current_page)}"}>
+          <a href=""><%= current_page %></a>
+         </.link>
         </div>
       </div>
 
       <div class="h-8 w-8 mr-1 flex justify-center items center cursor-pointer">
-        <a href="">
+        <.link patch={~p"/admin/products?#{Map.update(@options, :page, @options[:page], &(&1 + 1))}"}>
           <Heroicons.chevron_right solid class="h-6 w-6 stroke-current text-fuchsia-500" />
-        </a>
+        </.link>
       </div>
     </div>
     """
