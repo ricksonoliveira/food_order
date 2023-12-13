a997e464139abb3bc648ba202f0312cb7798bcfc
Rick G Oliveira <riksongleison471@gmail.com>
2023-12-13 12:59:48 -0300

    feat: add tests

---
+++ test/food_order_web/live/admin/product_live/index/paginate_test.exs
@@ -0,0 +1,61 @@
+defmodule FoodOrderWeb.Admin.ProductLive.PaginateTest do
+  use FoodOrderWeb.ConnCase, async: true
+  import FoodOrder.ProductsFixtures
+  import Phoenix.LiveViewTest
+
+  describe "paginate component" do
+    setup [:register_and_log_in_admin]
+
+    test "click next, previews and page", %{conn: conn} do
+      # Visit the admin products page
+      {:ok, lv, _html} = live(conn, ~p"/admin/products")
+
+      # Assert that the pagination component is present
+      assert has_element?(lv, "#pagination")
+
+      # Assert that clicking next, previews and page works
+      lv
+      |> element("[data-role=next]")
+      |> render_click()
+
+      assert_patched(
+        lv,
+        ~p"/admin/products?name=&sort_by=updated_at&page=2&per_page=4&sort_order=desc"
+      )
+
+      lv
+      |> element("[data-role=previous]")
+      |> render_click()
+
+      assert_patched(
+        lv,
+        ~p"/admin/products?name=&sort_by=updated_at&page=1&per_page=4&sort_order=desc"
+      )
+
+      lv
+      |> element("#pagination>div>div>a", "2")
+      |> render_click()
+
+      assert_patched(
+        lv,
+        ~p"/admin/products?name=&sort_by=updated_at&page=2&per_page=4&sort_order=desc"
+      )
+    end
+
+    test "using params", %{conn: conn} do
+      p1 = product_fixture()
+      p2 = product_fixture(name: "Product 2")
+
+      # Visit the admin products page
+      {:ok, lv, _html} =
+        live(
+          conn,
+          ~p"/admin/products?name=&page=1&per_page=1&sort_by=inserted_at&sort_order=desc"
+        )
+
+      # Assert renders the first product is present and the second isn't
+      assert has_element?(lv, "#products-#{p1.id}")
+      refute has_element?(lv, "#products-#{p2.id}")
+    end
+  end
+end
