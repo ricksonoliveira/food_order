c4e0cd7b4faf814d0277a6d929875e0f31756d9b
Rickson G. Oliveira <riksongleison471@gmail.com>
2023-12-13 18:16:52 -0300

    Feat: Infinite scroll (#15)
    * feat: hook for infinit scroll
    
    * feat: add tests for infinite scroll hook

---
+++ assets/js/hooks/loadMoreProducts.js
@@ -0,0 +1,17 @@
+const LoadMoreProducts = {
+	mounted() {
+		// console.log(this.el.dataset.page)	
+		const selector = "#" + this.el.id;
+		this.observer = new IntersectionObserver((entries) => {
+			const entry = entries[0]
+			if (entry.isIntersecting) {
+				console.log("load more")
+				this.pushEventTo(selector, "load_more_products", {})
+			}
+		})
+
+		this.observer.observe(this.el)
+	}
+}
+
+export default LoadMoreProducts;
--- lib/food_order_web/live/page_live/page_live.ex
+++ lib/food_order_web/live/page_live/page_live.ex
@@ -4,6 +4,27 @@ defmodule FoodOrderWeb.PageLive do
   alias FoodOrderWeb.PageLive.Item
 
   def mount(_, _, socket) do
-    {:ok, assign(socket, products: Products.list_products())}
+    socket =
+      socket
+      |> assign(page: 1, per_page: 8)
+      |> assign_list_products()
+
+    {:ok, socket}
+  end
+
+  def handle_event("load_more_products", _params, socket) do
+    socket =
+      socket
+      |> update(:page, &(&1 + 1))
+      |> assign_list_products()
+
+    {:noreply, socket}
+  end
+
+  defp assign_list_products(socket) do
+    page = socket.assigns.page
+    per_page = socket.assigns.per_page
+    paginate = [{:paginate, %{page: page, per_page: per_page}}]
+    assign(socket, products: Products.list_products(paginate))
   end
 end
