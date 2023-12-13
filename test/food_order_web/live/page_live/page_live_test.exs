9a3316c77240680dae2e30dc945df50b0a9f1d69
Rick G Oliveira <riksongleison471@gmail.com>
2023-12-13 18:13:24 -0300

    feat: add tests for infinite scroll hook

--- test/food_order_web/live/page_live/page_live_test.exs
+++ test/food_order_web/live/page_live/page_live_test.exs
@@ -4,14 +4,10 @@ defmodule FoodOrderWeb.PageLiveTest do
   import Phoenix.LiveViewTest
   import FoodOrder.ProductsFixtures
 
-  setup %{conn: conn} do
-    product = product_fixture()
+  test "load main hero html", %{conn: conn} do
+    _product = product_fixture()
     {:ok, view, _html} = live(conn, ~p"/")
 
-    {:ok, %{conn: conn, view: view, product: product}}
-  end
-
-  test "load main hero html", %{view: view} do
     hero_cta = "[data-role=hero-cta]"
     assert has_element?(view, "[data-role=hero]")
     assert has_element?(view, hero_cta)
@@ -22,13 +18,19 @@ defmodule FoodOrderWeb.PageLiveTest do
     assert has_element?(view, "[data-role=hero-img]")
   end
 
-  test "load products html", %{view: view} do
+  test "load products html", %{conn: conn} do
+    _product = product_fixture()
+    {:ok, view, _html} = live(conn, ~p"/")
+
     assert has_element?(view, "[data-role=products-section]")
     assert has_element?(view, "[data-role=products-section]>h1", "All foods")
     assert has_element?(view, "[data-role=products-list]")
   end
 
-  test "load main item elements", %{view: view, product: product} do
+  test "load main item elements", %{conn: conn} do
+    product = product_fixture()
+    {:ok, view, _html} = live(conn, ~p"/")
+
     assert has_element?(view, "[data-role=item][data-id=#{product.id}]")
     assert has_element?(view, "[data-role=item][data-id=#{product.id}]>img")
     assert has_element?(view, "[data-role=item-details][data-id=#{product.id}]>h2", product.name)
@@ -48,5 +50,45 @@ defmodule FoodOrderWeb.PageLiveTest do
     assert view
            |> element("[data-role=item-details][data-id=#{product.id}]>div>button")
            |> render() =~ "Add"
+  end
+
+  test "infinite scroll loads more products when triggered", %{conn: conn} do
+    products = for _ <- 0..12, do: product_fixture()
+
+    {:ok, lv, _html} = live(conn, ~p"/")
+
+    # We need to split the products into two pages, because the first page
+    # is already loaded when the live view is mounted.
+    [product_page_1, product_page_2] = Enum.chunk_every(products, 8)
+
+    # Assert that the first page of products is loaded.
+    Enum.each(product_page_1, fn product ->
+      assert has_element?(lv, "[data-role=item][data-id=#{product.id}]")
+    end)
+
+    # Assert that the second page of products is not loaded.
+    Enum.each(product_page_2, fn product ->
+      refute has_element?(lv, "[data-role=item][data-id=#{product.id}]")
+    end)
+
+    # Trigger the load more products hook event.
+    # Trigger twice to make sure that the hook is executed.
+    lv
+    |> element("#load_more_products")
+    |> render_hook("load_more_products", %{})
+
+    lv
+    |> element("#load_more_products")
+    |> render_hook("load_more_products", %{})
+
+    # Assert that the second page of products is loaded.
+    Enum.each(product_page_2, fn product ->
+      assert has_element?(lv, "[data-role=item][data-id=#{product.id}]")
+    end)
+
+    # Assert that the first page of products is still loaded as well.
+    Enum.each(product_page_1, fn product ->
+      assert has_element?(lv, "[data-role=item][data-id=#{product.id}]")
+    end)
   end
 end
