76ee9d7cd8071f9062f15c7f09d44c3b45deedf9
Rick G Oliveira <riksongleison471@gmail.com>
2023-12-13 23:33:45 -0300

    feat: cart item live view

--- lib/food_order_web/live/cart_live/cart_live.ex
+++ lib/food_order_web/live/cart_live/cart_live.ex
@@ -1,9 +1,16 @@
 defmodule FoodOrderWeb.CartLive do
   use FoodOrderWeb, :live_view
+
+  alias FoodOrder.{Carts, Products}
   alias FoodOrderWeb.CartLive.Details
 
   def mount(_, _, socket) do
-    {:ok, assign(socket, total_quantity: 0)}
+    uuid = Ecto.UUID.generate()
+    Carts.create(uuid)
+    product = Products.list_products() |> hd
+    Carts.add(uuid, product)
+    cart = Carts.get(uuid)
+    {:ok, assign(socket, cart: cart)}
   end
 
   defp empty_cart(assigns) do
